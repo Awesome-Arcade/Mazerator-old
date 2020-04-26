f00b985fac5e96c2d180711f1cb3c43093d1956d
AwesomeKalin <47223207+AwesomeKalin@users.noreply.github.com>
2020-04-26 12:35:09 +0100

    More Level 1ness
    Nearly finished correct path

--- Build Scripts/BuildWin64.bat
+++
@@ -1,7 +0,0 @@
-cd C:\Program Files\Epic Games\UE_4.23\Engine\Build\BatchFiles
-RunUAT BuildCookRun -project="C:\Users\Kalin\Documents\GitHub\Mazerator\Mazerator.uproject"^
- -noP4 -platform=Win64^
- -clientconfig=Shipping -serverconfig=Shipping^
- -cook -allmaps -build -stage^
- -pak -archive -archivedirectory="C:\Users\Kalin\Documents\GitHub\Mazerator\Builds\Windows"
-pause
